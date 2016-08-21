3aa93c9bb9617bbc86dca50d8b8243cb7c576eb4
Nick Coleman <nick@itprojmgt.com>
2016-08-21 20:22:57 +0000

    Moved SkiResort Schema to models directory

--- app.js
+++ app.js
@@ -3,39 +3,16 @@ var   express     = require("express"),
       bodyParser  = require("body-parser"),
       mongoose    = require('mongoose');
 
-mongoose.connect("mongodb://localhost/reviews");
+// Models
+var   SkiResort   = require('./models/skiresorts.js'),
+      Comments    = require('./models/comments.js');
 
+
+// Setup
+mongoose.connect("mongodb://localhost/reviews");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-// Schema
-var skiresortSchema = new mongoose.Schema({
-   name: String,
-   image: String,
-   description: String
-});
-
-var SkiResort = mongoose.model("SkiResort", skiresortSchema);
-
-// SkiResort.create({
-//    name: 'Deer Valley', 
-//    image: 'https://farm3.staticflickr.com/2215/2180936615_6601d11bc1.jpg',
-//    description: 'Deer Valley continually ranks #1 in the US for a reason ... great service and grooming'
-// },function(err, resort){
-//    if(err) {
-//       console.log(err);
-//       return;
-//    }
-//    console.log(resort);
-// });
-
-// var skiresorts = [
-//    {name: 'Deer Valley', image: 'https://farm3.staticflickr.com/2215/2180936615_6601d11bc1.jpg'},
-//    {name: 'Park City Mountain', image: 'https://farm3.staticflickr.com/2335/2180937479_b57ef35dce.jpg'},
-//    {name: 'Alta', image: 'https://farm3.staticflickr.com/2304/2181723762_4bc421c547.jpg'},
-//    {name: 'Solitude', image: 'http://snowbrains.com/wp-content/uploads/2014/10/xlarge.jpg', description='Just 30 minutes from Salt Lake City and with over 1200 acres… one of Utah's most intimate mountain resorts'}
-// ];
-   
 
 // ======== ROUTES =============   
 app.get("/", function(req, res){
---
+++ testData.md
@@ -0,0 +1,22 @@
+
+// Schema
+
+
+// SkiResort.create({
+//    name: 'Deer Valley', 
+//    image: 'https://farm3.staticflickr.com/2215/2180936615_6601d11bc1.jpg',
+//    description: 'Deer Valley continually ranks #1 in the US for a reason ... great service and grooming'
+// },function(err, resort){
+//    if(err) {
+//       console.log(err);
+//       return;
+//    }
+//    console.log(resort);
+// });
+
+// var skiresorts = [
+//    {name: 'Deer Valley', image: 'https://farm3.staticflickr.com/2215/2180936615_6601d11bc1.jpg'},
+//    {name: 'Park City Mountain', image: 'https://farm3.staticflickr.com/2335/2180937479_b57ef35dce.jpg'},
+//    {name: 'Alta', image: 'https://farm3.staticflickr.com/2304/2181723762_4bc421c547.jpg'},
+//    {name: 'Solitude', image: 'http://snowbrains.com/wp-content/uploads/2014/10/xlarge.jpg', description='Just 30 minutes from Salt Lake City and with over 1200 acres… one of Utah's most intimate mountain resorts'}
+// ];
