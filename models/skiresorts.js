a9e962dc395b50b12246326fdbcce74cdd42a27c
Nick Coleman <nick@itprojmgt.com>
2016-08-23 22:00:17 +0000

    Added model/comments.js and setup comment schema to add comments to each ski resort

--- models/skiresorts.js
+++ models/skiresorts.js
@@ -1,9 +1,16 @@
 var mongoose = require("mongoose");
+// var Comment =  require("./comments");
 
 var skiresortSchema = new mongoose.Schema({
    name: String,
    image: String,
-   description: String
+   description: String,
+   comments: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: 'Comment'
+      }
+   ]
 });
 
 module.exports = mongoose.model('SkiResort', skiresortSchema);
